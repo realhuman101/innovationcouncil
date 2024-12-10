d575982f73b4353f1eca710d77868362a32a702d
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-12-10 23:27:50 +0800

    add deploy info

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/innovationcouncil/' : '',
+  basePath: isProd ? '/innovationcouncil' : '',
+  output: 'export'
 };
 
 export default nextConfig;
