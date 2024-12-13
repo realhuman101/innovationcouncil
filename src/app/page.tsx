04fda2c94f2431ed8c15a91217304dd64ef8d949
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-12-14 01:28:20 +0800

    fix up homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,240 +2,33 @@ import styles from "./page.module.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
+import purposeImg from '@/imgs/purpose.webp'
+
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Header/>
+	return (
+		<div className={styles.page}>
+			<Header/>
 
-      <section id={styles.hero} className="blueBg">
-        <video src="/heroVideo.mp4" disablePictureInPicture playsInline autoPlay loop muted/>
-        <div id={styles.heroTitles}>
-          <h1>Student</h1>
-          <h1>Innovation</h1>
-          <h1>Council</h1>
-        </div>
-        <button>Join Us Now!</button>
-      </section>
+			<section id={styles.hero} className="blueBg">
+				<video src="/heroVideo.mp4" disablePictureInPicture playsInline autoPlay loop muted/>
+				<div id={styles.heroTitles}>
+					<h1>Student</h1>
+					<h1>Innovation</h1>
+					<h1>Council</h1>
+				</div>
+				<button>Join Us Now!</button>
+			</section>
 
-      <section id={styles.purpose}>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>Our Purpose</h1>
-          <p>sdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-         <div>
-          <h1>asdf</h1>
-          <p>asdf</p>
-         </div>
-      </section>
-      {/* <Footer/> */}
-    </div>
-  );
+			<section id={styles.purpose} style={{backgroundImage: `url(${purposeImg.src})`}}>
+					<div>
+						<div>
+							<h1>Our Purpose</h1>
+							<p>sdf</p>
+						</div>
+					</div>
+					<div></div>
+			</section>
+			{/* <Footer/> */}
+		</div>
+	);
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,23 @@
 "use client"
 
-import React, { useLayoutEffect, useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import LogoImg from '@/imgs/logo.png'
 
 function Header() {
 	const [state, changeState] = useState(false);
-	const [size, setSize] = useState([0, 0]);
-	const [hState, setHState] = useState(false);
+	const [size, setSize] = useState([845, 0]);
+	const hRef = useRef(null);
 
-	window.addEventListener('scroll', () => {changeState((document.documentElement.clientHeight ? document.documentElement : document.body).scrollTop != 0)});
-
-	useLayoutEffect(() => {
+	useEffect( () => {
 		function updateSize() {
 			setSize([window.innerWidth, window.innerHeight]);
 		}
+		
+		updateSize()
+		
+		window.addEventListener('scroll', () => {changeState((document.documentElement.clientHeight ? document.documentElement : document.body).scrollTop != 0)});
+
 		window.addEventListener('resize', updateSize);
 	}, []);
 
@@ -26,11 +29,13 @@ function Header() {
 			style={state ? {
 				backgroundColor: "rgba(255, 255, 255, 0.8)",
 				color: "var(--foreground)",
-				backdropFilter: "blur(10px)"
+				backdropFilter: "blur(10px)",
+				boxShadow: "rgba(0, 0, 0, 0.05) 0px 5px 10px;"
 			} : {
 				backgroundColor: "transparent",
 				color: "var(--background)",
 				backdropFilter: "none",
+				boxShadow: "none"
 			}}
 		>
 			<div>
@@ -53,12 +58,15 @@ function Header() {
 			<div>
 				<div id="hamburger">
 					<label id="hamburgerIcon">
-						<input type="checkbox" />
+						<input ref={hRef} type="checkbox"/>
 						<svg viewBox="0 0 32 32">
 							<path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
 							<path className="line" d="M7 16 27 16"></path>
 						</svg>
 					</label>
+
+					{/* @ts-expect-error stfu */}
+					<p>{hRef.current != null? hRef.current.checked : null}</p>
 				</div>
 			</div>
 		</div>
