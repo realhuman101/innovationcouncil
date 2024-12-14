ac5e655ebb70878d8a90976b72e98fa95b93dee8
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-12-14 13:17:58 +0800

    add better animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ function Header() {
 				backgroundColor: "rgba(255, 255, 255, 0.8)",
 				color: "var(--foreground)",
 				backdropFilter: "blur(10px)",
-				boxShadow: "rgba(0, 0, 0, 0.05) 0px 5px 10px;"
+				boxShadow: "rgba(0, 0, 0, 0.05) 0px 5px 10px"
 			} : {
 				backgroundColor: "transparent",
 				color: "var(--background)",
---
+++ src/components/Loader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loader() {
+  return (
+	<div id="loader">
+		<svg viewBox="25 25 50 50">
+			<circle r="20" cy="50" cx="50"></circle>
+		</svg>
+	</div>
+  )
+}
+
+export default Loader
